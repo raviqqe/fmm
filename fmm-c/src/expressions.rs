50455fe2eedb428340ef626c23ed241ded8fc675
Yota Toyama <raviqqe@gmail.com>
2021-01-07 03:10:11 +0000

    Generate type names

--- fmm-c/src/expressions.rs
+++ fmm-c/src/expressions.rs
@@ -1,23 +1,36 @@
+use crate::names::*;
 use fmm::ir::*;
 use fmm::types::{self, Type};
 
 pub fn compile_expression(expression: &Expression) -> String {
     match expression {
-        Expression::Primitive(_) => todo!(),
+        Expression::Primitive(primitive) => compile_primitive(*primitive),
         Expression::Record(_) => todo!(),
-        Expression::Undefined(undefined) => compile_undefined(undefined).into(),
+        Expression::Undefined(undefined) => compile_undefined(undefined),
         Expression::Union(_) => todo!(),
-        Expression::Variable(_) => todo!(),
+        Expression::Variable(variable) => variable.name().into(),
     }
 }
 
-fn compile_undefined(undefined: &Undefined) -> &str {
+fn compile_undefined(undefined: &Undefined) -> String {
     match undefined.type_() {
-        Type::Function(_) => "NULL",
-        Type::Primitive(primitive) => compile_undefined_primitive(*primitive),
-        Type::Pointer(_) => "NULL",
-        Type::Record(_) => "{}",
-        Type::Union(_) => "{}",
+        Type::Function(_) => "NULL".into(),
+        Type::Primitive(primitive) => compile_undefined_primitive(*primitive).into(),
+        Type::Pointer(_) => "NULL".into(),
+        Type::Record(record) => format!("struct {}", generate_record_type_name(record)) + "{}",
+        Type::Union(union) => format!("union {}", generate_union_type_name(union)) + "{}",
+    }
+}
+
+fn compile_primitive(primitive: Primitive) -> String {
+    match primitive {
+        Primitive::Bool(bool) => format!("{}", bool),
+        Primitive::Float32(number) => format!("{}", number),
+        Primitive::Float64(number) => format!("{}", number),
+        Primitive::Integer8(number) => format!("{}", number),
+        Primitive::Integer32(number) => format!("{}", number),
+        Primitive::Integer64(number) => format!("{}", number),
+        Primitive::PointerInteger(number) => format!("{}", number),
     }
 }
 
--- fmm-c/src/names.rs
+++ fmm-c/src/names.rs
@@ -1,3 +1,7 @@
+use fmm::types;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 pub fn generate_argument_name(index: usize) -> String {
     format!("a{}", index)
 }
@@ -8,4 +12,20 @@ pub fn generate_record_element_name(index: usize) -> String {
 
 pub fn generate_union_member_name(index: usize) -> String {
     format!("m{}", index)
+}
+
+pub fn generate_record_type_name(record: &types::Record) -> String {
+    format!("t_{}", hash(record))
+}
+
+pub fn generate_union_type_name(union: &types::Union) -> String {
+    format!("t_{:x}", hash(union))
+}
+
+fn hash(hash: impl Hash) -> u64 {
+    let mut hasher = DefaultHasher::new();
+
+    hash.hash(&mut hasher);
+
+    hasher.finish()
 }
