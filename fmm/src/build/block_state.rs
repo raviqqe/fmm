c13a4c12fc0b45f3bc0162291930c09749bff8e2
Yota Toyama <raviqqe@gmail.com>
2021-01-14 07:18:19 +0000

    Use ref cell

--- fmm/src/build/block_state.rs
+++ fmm/src/build/block_state.rs
@@ -2,35 +2,31 @@ use super::names::*;
 use super::typed_expression::*;
 use crate::ir::*;
 use crate::types::{self, Type};
+use std::cell::RefCell;
 
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct BlockState {
-    instructions: Vec<Instruction>,
+    instructions: RefCell<Vec<Instruction>>,
 }
 
 impl BlockState {
     pub fn new() -> Self {
         Self {
-            instructions: vec![],
+            instructions: vec![].into(),
         }
     }
 
-    pub fn instructions(&self) -> &[Instruction] {
-        &self.instructions
-    }
-
-    pub fn allocate_heap(&mut self, type_: impl Into<Type>) -> TypedExpression {
+    pub fn allocate_heap(&self, type_: impl Into<Type>) -> TypedExpression {
         let name = generate_name();
         let type_ = type_.into();
 
-        self.instructions
-            .push(AllocateHeap::new(type_.clone(), &name).into());
+        self.add_instruction(AllocateHeap::new(type_.clone(), &name));
 
         TypedExpression::new(Variable::new(name), types::Pointer::new(type_))
     }
 
     pub fn arithmetic_operation(
-        &mut self,
+        &self,
         operator: ArithmeticOperator,
         lhs: impl Into<TypedExpression>,
         rhs: impl Into<TypedExpression>,
@@ -40,51 +36,48 @@ impl BlockState {
         let type_ = lhs.type_().to_primitive().unwrap();
         let name = generate_name();
 
-        self.instructions.push(
-            ArithmeticOperation::new(
-                type_,
-                operator,
-                lhs.expression().clone(),
-                rhs.expression().clone(),
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(ArithmeticOperation::new(
+            type_,
+            operator,
+            lhs.expression().clone(),
+            rhs.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_)
     }
 
-    pub fn atomic_load(&mut self, pointer: impl Into<TypedExpression>) -> TypedExpression {
+    pub fn atomic_load(&self, pointer: impl Into<TypedExpression>) -> TypedExpression {
         let pointer = pointer.into();
         let type_ = pointer.type_().to_pointer().unwrap().element().clone();
         let name = generate_name();
 
-        self.instructions
-            .push(AtomicLoad::new(type_.clone(), pointer.expression().clone(), &name).into());
+        self.add_instruction(AtomicLoad::new(
+            type_.clone(),
+            pointer.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_)
     }
 
     pub fn atomic_store(
-        &mut self,
+        &self,
         value: impl Into<TypedExpression>,
         pointer: impl Into<TypedExpression>,
     ) {
         let value = value.into();
         let pointer = pointer.into();
 
-        self.instructions.push(
-            AtomicStore::new(
-                value.type_().clone(),
-                value.expression().clone(),
-                pointer.expression().clone(),
-            )
-            .into(),
-        );
+        self.add_instruction(AtomicStore::new(
+            value.type_().clone(),
+            value.expression().clone(),
+            pointer.expression().clone(),
+        ));
     }
 
     pub fn call(
-        &mut self,
+        &self,
         function: impl Into<TypedExpression>,
         arguments: impl IntoIterator<Item = TypedExpression>,
     ) -> TypedExpression {
@@ -93,25 +86,22 @@ impl BlockState {
         let type_ = function.type_().to_function().unwrap().clone();
         let name = generate_name();
 
-        self.instructions.push(
-            Call::new(
-                type_.clone(),
-                function.expression().clone(),
-                arguments
-                    .iter()
-                    .map(|typed_expression| typed_expression.expression())
-                    .cloned()
-                    .collect(),
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(Call::new(
+            type_.clone(),
+            function.expression().clone(),
+            arguments
+                .iter()
+                .map(|typed_expression| typed_expression.expression())
+                .cloned()
+                .collect(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_.result().clone())
     }
 
     pub fn compare_and_swap(
-        &mut self,
+        &self,
         pointer: impl Into<TypedExpression>,
         old_value: impl Into<TypedExpression>,
         new_value: impl Into<TypedExpression>,
@@ -121,22 +111,19 @@ impl BlockState {
         let new_value = new_value.into();
         let name = generate_name();
 
-        self.instructions.push(
-            CompareAndSwap::new(
-                old_value.type_().clone(),
-                pointer.expression().clone(),
-                old_value.expression().clone(),
-                new_value.expression().clone(),
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(CompareAndSwap::new(
+            old_value.type_().clone(),
+            pointer.expression().clone(),
+            old_value.expression().clone(),
+            new_value.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), types::Primitive::Bool)
     }
 
     pub fn comparison_operation(
-        &mut self,
+        &self,
         operator: ComparisonOperator,
         lhs: impl Into<TypedExpression>,
         rhs: impl Into<TypedExpression>,
@@ -145,22 +132,19 @@ impl BlockState {
         let rhs = rhs.into();
         let name = generate_name();
 
-        self.instructions.push(
-            ComparisonOperation::new(
-                lhs.type_().to_primitive().unwrap(),
-                operator,
-                lhs.expression().clone(),
-                rhs.expression().clone(),
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(ComparisonOperation::new(
+            lhs.type_().to_primitive().unwrap(),
+            operator,
+            lhs.expression().clone(),
+            rhs.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), types::Primitive::Bool)
     }
 
     pub fn deconstruct_record(
-        &mut self,
+        &self,
         record: impl Into<TypedExpression>,
         element_index: usize,
     ) -> TypedExpression {
@@ -168,21 +152,18 @@ impl BlockState {
         let type_ = record.type_().to_record().unwrap().clone();
         let name = generate_name();
 
-        self.instructions.push(
-            DeconstructRecord::new(
-                type_.clone(),
-                record.expression().clone(),
-                element_index,
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(DeconstructRecord::new(
+            type_.clone(),
+            record.expression().clone(),
+            element_index,
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_.elements()[element_index].clone())
     }
 
     pub fn deconstruct_union(
-        &mut self,
+        &self,
         union: impl Into<TypedExpression>,
         member_index: usize,
     ) -> TypedExpression {
@@ -190,21 +171,18 @@ impl BlockState {
         let type_ = union.type_().to_union().unwrap().clone();
         let name = generate_name();
 
-        self.instructions.push(
-            DeconstructUnion::new(
-                type_.clone(),
-                union.expression().clone(),
-                member_index,
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(DeconstructUnion::new(
+            type_.clone(),
+            union.expression().clone(),
+            member_index,
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_.members()[member_index].clone())
     }
 
     pub fn if_(
-        &mut self,
+        &self,
         condition: impl Into<TypedExpression>,
         then: Block,
         else_: Block,
@@ -219,33 +197,33 @@ impl BlockState {
             types::Record::new(vec![]).into()
         };
 
-        self.instructions.push(
-            If::new(
-                type_.clone(),
-                condition.expression().clone(),
-                then,
-                else_,
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(If::new(
+            type_.clone(),
+            condition.expression().clone(),
+            then,
+            else_,
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_)
     }
 
-    pub fn load(&mut self, pointer: impl Into<TypedExpression>) -> TypedExpression {
+    pub fn load(&self, pointer: impl Into<TypedExpression>) -> TypedExpression {
         let pointer = pointer.into();
         let type_ = pointer.type_().to_pointer().unwrap().element().clone();
         let name = generate_name();
 
-        self.instructions
-            .push(Load::new(type_.clone(), pointer.expression().clone(), &name).into());
+        self.add_instruction(Load::new(
+            type_.clone(),
+            pointer.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_)
     }
 
     pub fn pointer_address(
-        &mut self,
+        &self,
         pointer: impl Into<TypedExpression>,
         offset: impl Into<TypedExpression>,
     ) -> TypedExpression {
@@ -254,21 +232,18 @@ impl BlockState {
         let type_ = pointer.type_().to_pointer().unwrap().clone();
         let name = generate_name();
 
-        self.instructions.push(
-            PointerAddress::new(
-                type_.clone(),
-                pointer.expression().clone(),
-                offset.expression().clone(),
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(PointerAddress::new(
+            type_.clone(),
+            pointer.expression().clone(),
+            offset.expression().clone(),
+            &name,
+        ));
 
         TypedExpression::new(Variable::new(name), type_)
     }
 
     pub fn record_address(
-        &mut self,
+        &self,
         pointer: impl Into<TypedExpression>,
         element_index: usize,
     ) -> TypedExpression {
@@ -283,15 +258,12 @@ impl BlockState {
             .clone();
         let name = generate_name();
 
-        self.instructions.push(
-            RecordAddress::new(
-                type_.clone(),
-                pointer.expression().clone(),
-                element_index,
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(RecordAddress::new(
+            type_.clone(),
+            pointer.expression().clone(),
+            element_index,
+            &name,
+        ));
 
         TypedExpression::new(
             Variable::new(name),
@@ -299,26 +271,19 @@ impl BlockState {
         )
     }
 
-    pub fn store(
-        &mut self,
-        value: impl Into<TypedExpression>,
-        pointer: impl Into<TypedExpression>,
-    ) {
+    pub fn store(&self, value: impl Into<TypedExpression>, pointer: impl Into<TypedExpression>) {
         let value = value.into();
         let pointer = pointer.into();
 
-        self.instructions.push(
-            Store::new(
-                value.type_().clone(),
-                value.expression().clone(),
-                pointer.expression().clone(),
-            )
-            .into(),
-        );
+        self.add_instruction(Store::new(
+            value.type_().clone(),
+            value.expression().clone(),
+            pointer.expression().clone(),
+        ));
     }
 
     pub fn union_address(
-        &mut self,
+        &self,
         pointer: impl Into<TypedExpression>,
         member_index: usize,
     ) -> TypedExpression {
@@ -333,15 +298,12 @@ impl BlockState {
             .clone();
         let name = generate_name();
 
-        self.instructions.push(
-            UnionAddress::new(
-                type_.clone(),
-                pointer.expression().clone(),
-                member_index,
-                &name,
-            )
-            .into(),
-        );
+        self.add_instruction(UnionAddress::new(
+            type_.clone(),
+            pointer.expression().clone(),
+            member_index,
+            &name,
+        ));
 
         TypedExpression::new(
             Variable::new(name),
@@ -353,7 +315,7 @@ impl BlockState {
         let typed_expression = typed_expression.into();
 
         Block::new(
-            self.instructions.iter().cloned(),
+            self.instructions.borrow().iter().cloned(),
             Branch::new(
                 typed_expression.type_().clone(),
                 typed_expression.expression().clone(),
@@ -365,7 +327,7 @@ impl BlockState {
         let typed_expression = typed_expression.into();
 
         Block::new(
-            self.instructions.iter().cloned(),
+            self.instructions.borrow().iter().cloned(),
             Return::new(
                 typed_expression.type_().clone(),
                 typed_expression.expression().clone(),
@@ -375,5 +337,9 @@ impl BlockState {
 
     pub fn unreachable(&self) -> Block {
         Block::new(vec![], TerminalInstruction::Unreachable)
+    }
+
+    fn add_instruction(&self, instruction: impl Into<Instruction>) {
+        self.instructions.borrow_mut().push(instruction.into());
     }
 }
