15ed2d2a097ce66df57e83d21ea2187b946c9937
Yota Toyama <raviqqe@gmail.com>
2021-01-14 02:56:58 +0000

    Refactor

--- fmm/src/build/names.rs
+++ fmm/src/build/names.rs
@@ -2,6 +2,6 @@ use std::sync::atomic::{AtomicU64, Ordering};
 
 static NAME_ID: AtomicU64 = AtomicU64::new(0);
 
-pub fn generate_name() -> String {
+pub(crate) fn generate_name() -> String {
     format!("_fmm_{:x}", NAME_ID.fetch_add(1, Ordering::SeqCst))
 }
