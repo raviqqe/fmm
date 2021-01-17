1d510452243020dbf51d1b5836a6eff4c03c462c
Yota Toyama <raviqqe@gmail.com>
2021-01-17 00:18:26 +0000

    Add define_variable method

--- fmm/src/build/module_state.rs
+++ fmm/src/build/module_state.rs
@@ -8,20 +8,73 @@ use std::rc::Rc;
 
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct ModuleState {
+    variable_declarations: Rc<RefCell<Vec<VariableDeclaration>>>,
+    function_declarations: Rc<RefCell<Vec<FunctionDeclaration>>>,
+    variable_definitions: Rc<RefCell<Vec<VariableDefinition>>>,
     function_definitions: Rc<RefCell<Vec<FunctionDefinition>>>,
 }
 
 impl ModuleState {
     pub fn new() -> Self {
-        Self::from_function_definitions(RefCell::new(vec![]).into())
+        Self {
+            variable_declarations: RefCell::new(vec![]).into(),
+            function_declarations: RefCell::new(vec![]).into(),
+            variable_definitions: RefCell::new(vec![]).into(),
+            function_definitions: RefCell::new(vec![]).into(),
+        }
     }
 
-    fn from_function_definitions(
-        function_definitions: Rc<RefCell<Vec<FunctionDefinition>>>,
-    ) -> Self {
-        Self {
-            function_definitions,
-        }
+    pub fn declare_variable(
+        &self,
+        name: impl Into<String>,
+        type_: impl Into<Type>,
+    ) -> TypedExpression {
+        let name = name.into();
+        let type_ = type_.into();
+
+        self.variable_declarations
+            .borrow_mut()
+            .push(VariableDeclaration::new(&name, type_.clone()));
+
+        TypedExpression::new(Variable::new(name), types::Pointer::new(type_.clone()))
+    }
+
+    pub fn declare_function(
+        &self,
+        name: impl Into<String>,
+        type_: types::Function,
+    ) -> TypedExpression {
+        let name = name.into();
+
+        self.function_declarations
+            .borrow_mut()
+            .push(FunctionDeclaration::new(&name, type_.clone()));
+
+        TypedExpression::new(Variable::new(name), type_.clone())
+    }
+
+    pub fn define_variable(
+        &self,
+        name: impl Into<String>,
+        body: impl Into<TypedExpression>,
+        mutable: bool,
+    ) -> TypedExpression {
+        let name = name.into();
+        let body = body.into();
+
+        self.variable_definitions
+            .borrow_mut()
+            .push(VariableDefinition::new(
+                &name,
+                body.expression().clone(),
+                body.type_().clone(),
+                mutable,
+            ));
+
+        TypedExpression::new(
+            Variable::new(name),
+            types::Pointer::new(body.type_().clone()),
+        )
     }
 
     pub fn define_function(
@@ -39,9 +92,7 @@ impl ModuleState {
             .push(FunctionDefinition::new(
                 &name,
                 arguments.clone(),
-                body(BlockState::new(Self::from_function_definitions(
-                    self.function_definitions.clone(),
-                ))),
+                body(BlockState::new(self.clone())),
                 result_type.clone(),
             ));
 
