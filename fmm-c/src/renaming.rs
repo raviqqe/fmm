c5bc0caa82c7458930149b81e619907b24001b7c
Yota Toyama <raviqqe@gmail.com>
2021-01-24 06:46:35 +0000

    Fix renaming

--- fmm-c/src/renaming.rs
+++ fmm-c/src/renaming.rs
@@ -2,8 +2,65 @@ use fmm::ir::*;
 
 pub fn rename_names(module: &Module) -> Module {
     fmm::analysis::rename_names(module, |name| {
-        name.chars()
-            .map(|char| if char.is_alphanumeric() { char } else { '_' })
-            .collect()
+        if name == "" {
+            "_".into()
+        } else {
+            name.chars()
+                .map(|char| if char.is_alphanumeric() { char } else { '_' })
+                .collect()
+        }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rename_empty_name() {
+        assert_eq!(
+            rename_names(&Module::new(
+                vec![VariableDeclaration::new(
+                    "",
+                    fmm::types::Primitive::PointerInteger
+                )],
+                vec![],
+                vec![],
+                vec![]
+            )),
+            Module::new(
+                vec![VariableDeclaration::new(
+                    "_",
+                    fmm::types::Primitive::PointerInteger
+                )],
+                vec![],
+                vec![],
+                vec![]
+            )
+        );
+    }
+
+    #[test]
+    fn rename_invalid_name() {
+        assert_eq!(
+            rename_names(&Module::new(
+                vec![VariableDeclaration::new(
+                    "$x",
+                    fmm::types::Primitive::PointerInteger
+                )],
+                vec![],
+                vec![],
+                vec![]
+            )),
+            Module::new(
+                vec![VariableDeclaration::new(
+                    "_x",
+                    fmm::types::Primitive::PointerInteger
+                )],
+                vec![],
+                vec![],
+                vec![]
+            )
+        );
+    }
 }
