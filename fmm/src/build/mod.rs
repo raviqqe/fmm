9ab34ea39d1685492a8933831730df1806b3a561
Yota Toyama <raviqqe@gmail.com>
2021-02-21 06:48:11 +0000

    Add builder utilities

--- fmm/src/build/mod.rs
+++ fmm/src/build/mod.rs
@@ -1,7 +1,9 @@
 mod block_builder;
 mod module_builder;
 mod typed_expression;
+mod utilities;
 
 pub use block_builder::*;
 pub use module_builder::*;
 pub use typed_expression::*;
+pub use utilities::*;
