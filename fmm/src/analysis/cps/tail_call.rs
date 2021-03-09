048aa561ee5c56f164f66dd7e67bbe748355f46a
Yota Toyama <raviqqe@gmail.com>
2021-03-09 16:53:17 +0000

    Add todo comment

--- fmm/src/analysis/cps/tail_call.rs
+++ fmm/src/analysis/cps/tail_call.rs
@@ -1,5 +1,6 @@
 use crate::ir::*;
 
+// TODO Consider removing or canonicalizing pass-through instructions.
 pub fn is_tail_call(instructions: &[Instruction]) -> bool {
     instructions
         .iter()
