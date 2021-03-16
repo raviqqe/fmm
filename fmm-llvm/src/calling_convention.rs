a8a54411aa8972c119f467bd51422c63c38fe31a
Yota Toyama <raviqqe@gmail.com>
2021-03-16 06:58:00 +0000

    Use tailcc

--- fmm-llvm/src/calling_convention.rs
+++ fmm-llvm/src/calling_convention.rs
@@ -1,8 +1,8 @@
 pub fn compile_calling_convention(calling_convention: fmm::types::CallingConvention) -> u32 {
-    (match calling_convention {
+    match calling_convention {
         fmm::types::CallingConvention::Source | fmm::types::CallingConvention::Target => {
-            llvm_sys::LLVMCallConv::LLVMCCallConv
+            llvm_sys::LLVMCallConv::LLVMCCallConv as u32
         }
-        fmm::types::CallingConvention::Tail => llvm_sys::LLVMCallConv::LLVMFastCallConv,
-    }) as u32
+        fmm::types::CallingConvention::Tail => 18, // llvm/include/llvm/IR/CallingConv.h
+    }
 }
