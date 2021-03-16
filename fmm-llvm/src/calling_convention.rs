d2ae14e3bda3e953d3018a67781d3011a9f8fd51
Yota Toyama <raviqqe@gmail.com>
2021-03-16 06:59:16 +0000

    Update comment

--- fmm-llvm/src/calling_convention.rs
+++ fmm-llvm/src/calling_convention.rs
@@ -3,6 +3,7 @@ pub fn compile_calling_convention(calling_convention: fmm::types::CallingConvent
         fmm::types::CallingConvention::Source | fmm::types::CallingConvention::Target => {
             llvm_sys::LLVMCallConv::LLVMCCallConv as u32
         }
-        fmm::types::CallingConvention::Tail => 18, // llvm/include/llvm/IR/CallingConv.h
+        // tailcc. See llvm/include/llvm/IR/CallingConv.h.
+        fmm::types::CallingConvention::Tail => 18,
     }
 }
