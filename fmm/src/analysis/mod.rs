3517abeddf4ca6db23fd06aa18cd31dcd6bdb021
Yota Toyama <raviqqe@gmail.com>
2021-03-20 20:28:36 +0000

    Add reference_count module

--- fmm/src/analysis/mod.rs
+++ fmm/src/analysis/mod.rs
@@ -1,5 +1,6 @@
 mod cps;
 mod expression_conversion;
+mod reference_count;
 mod renaming;
 mod type_check;
 mod type_collection;
@@ -7,6 +8,7 @@ mod type_conversion;
 
 pub use cps::*;
 pub use expression_conversion::*;
+pub use reference_count::*;
 pub use renaming::*;
 pub use type_check::*;
 pub use type_collection::*;
