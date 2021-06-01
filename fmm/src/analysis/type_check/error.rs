9f7b0e9f2d153dd1996d3d7ace4f6813f4464568
Yota Toyama <raviqqe@gmail.com>
2021-06-01 04:52:08 +0000

    Validate duplicate names

--- fmm/src/analysis/type_check/error.rs
+++ fmm/src/analysis/type_check/error.rs
@@ -2,6 +2,7 @@ use crate::{ir::*, types::Type};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum TypeCheckError {
+    DuplicateNames(String),
     FunctionArguments(Call),
     IndexOutOfRange,
     InvalidBranch(Branch),
