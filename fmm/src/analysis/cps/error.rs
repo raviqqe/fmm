9d8a5a31568265a54d689b749f4577ee03944f78
Yota Toyama <raviqqe@gmail.com>
2021-06-17 03:59:43 +0000

    Refactor

--- fmm/src/analysis/cps/error.rs
+++ fmm/src/analysis/cps/error.rs
@@ -1,13 +1,31 @@
+use super::super::TypeCheckError;
 use crate::{build::BuildError, ir::*};
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum CpsTransformationError {
-    BuildError(BuildError),
+    Build(BuildError),
     InvalidCallingConvention(Call),
+    TypeCheck(TypeCheckError),
 }
+
+impl Display for CpsTransformationError {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "{:?}", self)
+    }
+}
+
+impl Error for CpsTransformationError {}
 
 impl From<BuildError> for CpsTransformationError {
     fn from(error: BuildError) -> Self {
-        Self::BuildError(error)
+        Self::Build(error)
+    }
+}
+
+impl From<TypeCheckError> for CpsTransformationError {
+    fn from(error: TypeCheckError) -> Self {
+        Self::TypeCheck(error)
     }
 }
--- fmm/src/analysis/type_check/error.rs
+++ fmm/src/analysis/type_check/error.rs
@@ -1,4 +1,6 @@
 use crate::{ir::*, types::Type};
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum TypeCheckError {
@@ -10,3 +12,11 @@ pub enum TypeCheckError {
     TypesNotMatched(Type, Type),
     VariableNotFound(Variable),
 }
+
+impl Display for TypeCheckError {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "{:?}", self)
+    }
+}
+
+impl Error for TypeCheckError {}
