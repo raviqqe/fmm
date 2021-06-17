23934eceae090e8488570e1680a689a3a6e00f12
Yota Toyama <raviqqe@gmail.com>
2021-06-16 21:06:30 -0700

    Refactor (#168)

--- fmm/src/analysis/type_check/error.rs
+++ fmm/src/analysis/type_check/error.rs
@@ -1,4 +1,8 @@
 use crate::{ir::*, types::Type};
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum TypeCheckError {
@@ -10,3 +14,11 @@ pub enum TypeCheckError {
     TypesNotMatched(Type, Type),
     VariableNotFound(Variable),
 }
+
+impl Display for TypeCheckError {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "{:?}", self)
+    }
+}
+
+impl Error for TypeCheckError {}
