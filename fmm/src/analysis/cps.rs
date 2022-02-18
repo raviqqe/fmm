eb66e159bb397fe0e5e0f94ca236fb5a16d95d4c
Yota Toyama <raviqqe@gmail.com>
2022-02-18 04:11:28 +0000

    Take snapshot

--- fmm/src/analysis/cps.rs
+++ fmm/src/analysis/cps.rs
@@ -35,7 +35,7 @@ pub fn transform_to_cps(
 mod tests {
     use super::*;
     use crate::{
-        analysis::check_types,
+        analysis::{check_types, format_module},
         types::{self, CallingConvention, Type, VOID_TYPE},
     };
     use stack::STACK_TYPE;
@@ -247,6 +247,51 @@ mod tests {
                 ),
                 types::Primitive::Float64,
             )],
+        ));
+    }
+
+    #[test]
+    fn transform_if_with_branch_snapshot() {
+        let function_type = create_function_type(
+            vec![types::Primitive::Float64.into()],
+            types::Primitive::Float64,
+        );
+
+        insta::assert_snapshot!(format_module(
+            &transform_to_cps(
+                &Module::new(
+                    vec![],
+                    vec![FunctionDeclaration::new("f", function_type.clone())],
+                    vec![],
+                    vec![create_function_definition(
+                        "g",
+                        vec![],
+                        Block::new(
+                            vec![If::new(
+                                types::Primitive::Float64,
+                                Primitive::Boolean(true),
+                                Block::new(
+                                    vec![Call::new(
+                                        function_type,
+                                        Variable::new("f"),
+                                        vec![Primitive::Float64(42.0).into()],
+                                        "x",
+                                    )
+                                    .into()],
+                                    Branch::new(types::Primitive::Float64, Variable::new("x")),
+                                ),
+                                Block::new(vec![], TerminalInstruction::Unreachable),
+                                "y",
+                            )
+                            .into()],
+                            Return::new(types::Primitive::Float64, Variable::new("y")),
+                        ),
+                        types::Primitive::Float64,
+                    )],
+                ),
+                VOID_TYPE.clone()
+            )
+            .unwrap()
         ));
     }
 
