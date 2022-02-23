ca0cf701dec03f6d244aeb1f9061d82c20830555
Yota Toyama <raviqqe@gmail.com>
2022-02-23 15:17:12 +0000

    Add todo

--- fmm-llvm/src/calling_convention.rs
+++ fmm-llvm/src/calling_convention.rs
@@ -7,6 +7,7 @@ pub fn compile_calling_convention(
         fmm::types::CallingConvention::Source | fmm::types::CallingConvention::Target => {
             Ok(llvm_sys::LLVMCallConv::LLVMCCallConv as u32)
         }
+        // TODO Use LLVMTailCallConv when it is added to the LLVM C API.
         fmm::types::CallingConvention::Tail => Ok(llvm_sys::LLVMCallConv::LLVMFastCallConv as u32),
         fmm::types::CallingConvention::Trampoline => Err(
             CompileError::UnsupportedCallingConvention(calling_convention),
