3205d4882048740a6e78cfee9b90e53a2beda0de
Yota Toyama <raviqqe@gmail.com>
2022-07-26 19:28:56 -0700

    Rename function back (#302)

--- fmm-c/src/lib.rs
+++ fmm-c/src/lib.rs
@@ -23,7 +23,7 @@ pub fn compile(
     module: &Module,
     instruction_configuration: Option<InstructionConfiguration>,
 ) -> Result<String, CompileError> {
-    fmm::analysis::check(module)?;
+    fmm::analysis::check_types(module)?;
 
     let module = renaming::rename(module);
     let global_variables = module
