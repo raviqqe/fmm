4722086ce9f000e3093df26e5d74d62fa087b952
Yota Toyama <raviqqe@gmail.com>
2022-08-25 19:17:55 -0700

    Optimize data structures more (#320)
    * Optimize more
    
    * Fix more

--- fmm/src/ir/block.rs
+++ fmm/src/ir/block.rs
@@ -1,8 +1,9 @@
 use super::{instruction::Instruction, terminal_instruction::TerminalInstruction};
+use std::sync::Arc;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Block {
-    instructions: Vec<Instruction>,
+    instructions: Arc<[Instruction]>,
     terminal_instruction: TerminalInstruction,
 }
 
@@ -12,7 +13,7 @@ impl Block {
         terminal_instruction: impl Into<TerminalInstruction>,
     ) -> Self {
         Self {
-            instructions: instructions.into_iter().collect(),
+            instructions: instructions.into(),
             terminal_instruction: terminal_instruction.into(),
         }
     }
