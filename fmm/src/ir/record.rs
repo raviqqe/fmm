11860b34289d7ff76b8113b1ec19bd018d8341e5
Yota Toyama <raviqqe@gmail.com>
2022-08-26 02:10:59 +0000

    Optimize more

--- fmm/src/ir/record.rs
+++ fmm/src/ir/record.rs
@@ -1,15 +1,19 @@
 use super::expression::Expression;
 use crate::types;
+use std::sync::Arc;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Record {
     type_: types::Record,
-    fields: Vec<Expression>,
+    fields: Arc<[Expression]>,
 }
 
 impl Record {
     pub fn new(type_: types::Record, fields: Vec<Expression>) -> Self {
-        Self { type_, fields }
+        Self {
+            type_,
+            fields: fields.into(),
+        }
     }
 
     pub fn type_(&self) -> &types::Record {
