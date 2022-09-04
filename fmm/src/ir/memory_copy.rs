e5ed00aafafbb99cb2d405a75ddc19b4e4174b2c
Yota Toyama <raviqqe@gmail.com>
2022-09-04 14:31:57 -0700

    Add builder

--- fmm/src/ir/memory_copy.rs
+++ fmm/src/ir/memory_copy.rs
@@ -4,20 +4,20 @@ use std::sync::Arc;
 #[derive(Clone, Debug, PartialEq)]
 pub struct MemoryCopy {
     source: Arc<Expression>,
-    length: Arc<Expression>,
     destination: Arc<Expression>,
+    size: Arc<Expression>,
 }
 
 impl MemoryCopy {
     pub fn new(
         source: impl Into<Expression>,
         destination: impl Into<Expression>,
-        length: impl Into<Expression>,
+        size: impl Into<Expression>,
     ) -> Self {
         Self {
             source: source.into().into(),
             destination: destination.into().into(),
-            length: length.into().into(),
+            size: size.into().into(),
         }
     }
 
@@ -30,6 +30,6 @@ impl MemoryCopy {
     }
 
     pub fn size(&self) -> &Expression {
-        &self.length
+        &self.size
     }
 }
