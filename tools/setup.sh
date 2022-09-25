86050d5fcd59bf946dbb9d10159f24ae2dcabb87
Yota Toyama <raviqqe@gmail.com>
2022-09-24 21:28:09 -0700

    Use LLVM 15 on CI

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-llvm_version=14
+llvm_version=15
 
 brew update
 brew install llvm@$llvm_version
-llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
+llvm_prefix=$(brew --prefix llvm@$llvm_version)
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
