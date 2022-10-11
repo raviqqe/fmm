13a059a42cd539a38bd8086192071e570b88eb99
Yota Toyama <raviqqe@gmail.com>
2022-10-10 19:50:55 -0700

    Update

--- fmm/src/analysis/c_calling_convention/type_.rs
+++ fmm/src/analysis/c_calling_convention/type_.rs
@@ -40,3 +40,110 @@ pub fn is_memory_class(context: &Context, type_: &Type) -> bool {
         Type::Function(_) | Type::Pointer(_) | Type::Primitive(_) => false,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const WORD_BYTES: usize = 8;
+
+    mod function {
+        use super::*;
+
+        #[test]
+        fn transform_function_of_source_calling_convention() {
+            let record = types::Record::new(vec![
+                types::Primitive::Integer64.into(),
+                types::Primitive::Integer64.into(),
+                types::Primitive::Integer64.into(),
+            ]);
+            let function =
+                types::Function::new(vec![], record.clone(), types::CallingConvention::Source);
+
+            assert_eq!(
+                transform_function(&Context::new(WORD_BYTES), &function),
+                function
+            );
+        }
+
+        #[test]
+        fn transform_compatible_function() {
+            let function = types::Function::new(
+                vec![],
+                types::Primitive::Integer64,
+                types::CallingConvention::Target,
+            );
+
+            assert_eq!(
+                transform_function(&Context::new(WORD_BYTES), &function),
+                function
+            );
+        }
+    }
+
+    mod memory_class {
+        use super::*;
+
+        #[test]
+        fn function() {
+            assert!(!is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Function::new(
+                    vec![],
+                    types::Primitive::PointerInteger,
+                    types::CallingConvention::Target
+                )
+                .into()
+            ));
+        }
+
+        #[test]
+        fn primitive() {
+            assert!(!is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Primitive::PointerInteger.into()
+            ));
+        }
+
+        #[test]
+        fn pointer() {
+            assert!(!is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Pointer::new(types::Primitive::PointerInteger).into()
+            ));
+        }
+
+        #[test]
+        fn empty_record() {
+            assert!(!is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Record::new(vec![]).into()
+            ));
+        }
+
+        #[test]
+        fn small_record() {
+            assert!(!is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Record::new(vec![
+                    types::Primitive::Integer64.into(),
+                    types::Primitive::Integer64.into()
+                ])
+                .into()
+            ));
+        }
+
+        #[test]
+        fn large_record() {
+            assert!(is_memory_class(
+                &Context::new(WORD_BYTES),
+                &types::Record::new(vec![
+                    types::Primitive::Integer64.into(),
+                    types::Primitive::Integer64.into(),
+                    types::Primitive::Integer64.into()
+                ])
+                .into()
+            ));
+        }
+    }
+}
