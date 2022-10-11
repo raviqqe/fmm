0b2bea1ef479aff872afc65aa0dda7c7c6f437a8
Yota Toyama <raviqqe@gmail.com>
2022-10-11 08:42:39 -0700

    Fix

--- fmm/src/analysis/c_calling_convention/call.rs
+++ fmm/src/analysis/c_calling_convention/call.rs
@@ -47,29 +47,55 @@ fn transform_instruction(
                     let builder = InstructionBuilder::new(Rc::new(
                         NameGenerator::new(format!("{}_c_", call.name())).into(),
                     ));
-                    let function_type = call.type_();
-                    let pointer = builder.allocate_stack(function_type.result().clone());
+                    let original_function_type = call.type_();
+                    let function_type = type_::transform_function(context, original_function_type);
+                    let function = build::variable(variable.name(), function_type.clone());
 
-                    builder.call(
-                        build::variable(
-                            variable.name(),
-                            type_::transform_function(context, function_type),
-                        ),
-                        call.arguments()
-                            .iter()
-                            .zip(function_type.arguments())
-                            .map(|(argument, type_)| {
-                                TypedExpression::new(argument.clone(), type_.clone())
-                            })
-                            .chain([pointer.clone()])
-                            .collect(),
-                    )?;
+                    let mut arguments = vec![];
 
-                    builder.add_instruction(Load::new(
-                        function_type.result().clone(),
-                        pointer.expression().clone(),
-                        call.name(),
-                    ));
+                    for (argument, type_) in call
+                        .arguments()
+                        .iter()
+                        .zip(original_function_type.arguments())
+                    {
+                        let argument = TypedExpression::new(argument.clone(), type_.clone());
+
+                        if type_::is_memory_class(context, type_) {
+                            let pointer = builder.allocate_stack(type_.clone());
+
+                            builder.store(argument, pointer.clone());
+
+                            arguments.push(pointer);
+                        } else {
+                            arguments.push(argument);
+                        }
+                    }
+
+                    if type_::is_memory_class(context, original_function_type.result()) {
+                        let pointer =
+                            builder.allocate_stack(original_function_type.result().clone());
+
+                        builder.call(
+                            function,
+                            arguments.into_iter().chain([pointer.clone()]).collect(),
+                        )?;
+
+                        builder.add_instruction(Load::new(
+                            original_function_type.result().clone(),
+                            pointer.expression().clone(),
+                            call.name(),
+                        ));
+                    } else {
+                        builder.add_instruction(Call::new(
+                            function_type,
+                            function.expression().clone(),
+                            arguments
+                                .into_iter()
+                                .map(|argument| argument.expression().clone())
+                                .collect(),
+                            call.name(),
+                        ));
+                    }
 
                     builder.into_instructions()
                 }
@@ -89,7 +115,73 @@ mod tests {
     const WORD_BYTES: usize = 8;
 
     #[test]
-    fn transform() {
+    fn transform_argument() {
+        let record_type = types::Record::new(vec![
+            types::Primitive::Integer64.into(),
+            types::Primitive::Integer64.into(),
+            types::Primitive::Integer64.into(),
+        ]);
+
+        assert_eq!(
+            transform_function_definition(
+                &Context::new(WORD_BYTES),
+                &FunctionDefinition::new(
+                    "f",
+                    vec![],
+                    types::Primitive::Integer64,
+                    Block::new(
+                        vec![Call::new(
+                            types::Function::new(
+                                vec![record_type.clone().into()],
+                                types::Primitive::Integer64,
+                                types::CallingConvention::Target,
+                            ),
+                            Variable::new("f"),
+                            vec![Undefined::new(record_type.clone()).into()],
+                            "x",
+                        )
+                        .into()],
+                        Return::new(types::Primitive::Integer64, Variable::new("x")),
+                    ),
+                    FunctionDefinitionOptions::new()
+                        .set_calling_convention(types::CallingConvention::Target),
+                )
+            ),
+            Ok(FunctionDefinition::new(
+                "f",
+                vec![],
+                types::Primitive::Integer64,
+                Block::new(
+                    vec![
+                        AllocateStack::new(record_type.clone(), "x_c_0").into(),
+                        Store::new(
+                            record_type.clone(),
+                            Undefined::new(record_type.clone()),
+                            Variable::new("x_c_0")
+                        )
+                        .into(),
+                        Call::new(
+                            types::Function::new(
+                                vec![types::Pointer::new(record_type.clone()).into()],
+                                types::Primitive::Integer64,
+                                types::CallingConvention::Target,
+                            ),
+                            Variable::new("f"),
+                            vec![Variable::new("x_c_0").into()],
+                            "x",
+                        )
+                        .into()
+                    ],
+                    Return::new(types::Primitive::Integer64, Variable::new("x")),
+                ),
+                FunctionDefinitionOptions::new()
+                    .set_calling_convention(types::CallingConvention::Target),
+            ))
+        );
+    }
+
+    #[test]
+    fn transform_result() {
         let record_type = types::Record::new(vec![
             types::Primitive::Integer64.into(),
             types::Primitive::Integer64.into(),
