cb8a36d591a52c903f9e83ca8e68b957973a1b93
Yota Toyama <raviqqe@gmail.com>
2022-10-15 15:42:32 -0700

    Refactor

--- fmm/src/analysis/cps/continuation_type.rs
+++ fmm/src/analysis/cps/continuation_type.rs
@@ -1,9 +1,9 @@
-use super::stack::type_;
+use super::stack;
 use crate::types::{self, CallingConvention, Type};
 
 pub fn compile(result_type: &Type, continuation_result_type: &Type) -> types::Function {
     types::Function::new(
-        vec![type_(), result_type.clone()],
+        vec![stack::type_(), result_type.clone()],
         continuation_result_type.clone(),
         CallingConvention::Tail,
     )
