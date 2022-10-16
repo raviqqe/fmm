e030db96a47e83d99582cafcf534398d3d6188bb
Yota Toyama <raviqqe@gmail.com>
2022-10-16 13:20:41 -0700

    Format files

--- fmm/src/analysis/cps/stack.rs
+++ fmm/src/analysis/cps/stack.rs
@@ -1,7 +1,4 @@
-use std::{
-    cell::{RefCell},
-    rc::Rc,
-};
+use std::{cell::RefCell, rc::Rc};
 
 use crate::{
     build::{self, BuildError, InstructionBuilder, NameGenerator, TypedExpression},
