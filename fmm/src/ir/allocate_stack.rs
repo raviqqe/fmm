c2248c0d2a2aac44dee371509d886d9e02caf191
Yota Toyama <raviqqe@gmail.com>
2022-10-17 19:57:18 -0700

    Fix

--- fmm/src/ir/allocate_stack.rs
+++ fmm/src/ir/allocate_stack.rs
@@ -1,24 +1,31 @@
 use crate::types::Type;
+use std::sync::Arc;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct AllocateStack {
+pub struct AllocateStack(Arc<AllocateStackInner>);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct AllocateStackInner {
     type_: Type,
     name: String,
 }
 
 impl AllocateStack {
     pub fn new(type_: impl Into<Type>, name: impl Into<String>) -> Self {
-        Self {
-            type_: type_.into(),
-            name: name.into(),
-        }
+        Self(
+            AllocateStackInner {
+                type_: type_.into(),
+                name: name.into(),
+            }
+            .into(),
+        )
     }
 
     pub fn type_(&self) -> &Type {
-        &self.type_
+        &self.0.type_
     }
 
     pub fn name(&self) -> &str {
-        &self.name
+        &self.0.name
     }
 }
