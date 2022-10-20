d2312f52f028395fcf4c8429dffcfed7a7d778a3
Yota Toyama <raviqqe@gmail.com>
2022-10-19 21:12:36 -0700

    Add validation module

--- fmm/src/analysis/validation.rs
+++ fmm/src/analysis/validation.rs
@@ -1,13 +1,29 @@
+use super::{
+    name::{self, NameError},
+    type_check::{self, TypeCheckError},
+};
 use crate::ir::Module;
 
-pub fn validate(module: &Module) -> Result<(), Validation> {
+pub fn validate(module: &Module) -> Result<(), ValidationError> {
     name::check(module)?;
     type_check::check(module)?;
 
     Ok(())
 }
 
-pub enum ValidateError {
+pub enum ValidationError {
     Name(NameError),
     TypeCheck(TypeCheckError),
+}
+
+impl From<NameError> for ValidationError {
+    fn from(error: NameError) -> Self {
+        Self::Name(error)
+    }
+}
+
+impl From<TypeCheckError> for ValidationError {
+    fn from(error: TypeCheckError) -> Self {
+        Self::TypeCheck(error)
+    }
 }
