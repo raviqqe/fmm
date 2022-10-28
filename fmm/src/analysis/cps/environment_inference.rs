90b0c7b8aeacced4a8b29b424c68555c9a272b00
Yota Toyama <raviqqe@gmail.com>
2022-10-27 23:06:54 -0700

    TEST

--- fmm/src/analysis/cps/environment_inference.rs
+++ fmm/src/analysis/cps/environment_inference.rs
@@ -80,6 +80,7 @@ fn collect_from_instruction(instruction: &mut Instruction, variables: &mut Index
                 transform_block(if_.else_mut(), variables);
             } else {
                 let mut other_variables = variables.clone();
+                dbg!(variables.len());
 
                 transform_block(if_.then_mut(), variables);
                 transform_block(if_.else_mut(), &mut other_variables);
