e387a913c0480e564cfdd5bd822583bc4a5eb4b8
Yota Toyama <raviqqe@gmail.com>
2022-10-29 18:44:54 -0700

    Fix if blocks

--- fmm/src/analysis/cps/environment_inference/sort.rs
+++ fmm/src/analysis/cps/environment_inference/sort.rs
@@ -24,22 +24,36 @@ fn transform_block(block: &mut Block, variables: &IndexMap<Rc<str>, usize>) {
     for instruction in block.instructions_mut().iter_mut().rev() {
         // Currently, we do not sort stack elements in if instructions as they do not
         // use stacks on memory.
-        if let Instruction::Call(call) = instruction {
-            if call.type_().calling_convention() == types::CallingConvention::Source {
-                call.environment_mut().sort_by(|one, other| {
-                    variable_order(one, variables).cmp(&variable_order(other, variables))
-                });
+        match instruction {
+            Instruction::Call(call) => {
+                if call.type_().calling_convention() == types::CallingConvention::Source {
+                    call.environment_mut().sort_by(|one, other| {
+                        variable_order(one, variables).cmp(&variable_order(other, variables))
+                    });
+                }
             }
+            Instruction::If(if_) => {
+                transform_block(if_.then_mut(), variables);
+                transform_block(if_.else_mut(), variables);
+            }
+            _ => {}
         }
     }
 }
 
 fn collect_from_block(block: &Block, variables: &mut IndexMap<Rc<str>, usize>) {
     for instruction in block.instructions() {
-        if let Instruction::Call(call) = instruction {
-            for name in call.environment() {
-                variables.insert(name.clone(), variables.get(name).copied().unwrap_or(0) + 1);
+        match instruction {
+            Instruction::Call(call) => {
+                for name in call.environment() {
+                    variables.insert(name.clone(), variables.get(name).copied().unwrap_or(0) + 1);
+                }
             }
+            Instruction::If(if_) => {
+                collect_from_block(if_.then(), variables);
+                collect_from_block(if_.else_(), variables);
+            }
+            _ => {}
         }
     }
 }
@@ -251,6 +265,142 @@ mod tests {
                             .into()
                         ],
                         Return::new(types::Primitive::PointerInteger, Variable::new("b")),
+                    ),
+                    Default::default(),
+                )],
+            )
+        );
+    }
+
+    #[test]
+    fn transform_two_free_variables_in_if() {
+        let function_type = types::Function::new(
+            vec![],
+            types::Primitive::PointerInteger,
+            types::CallingConvention::Source,
+        );
+
+        assert_eq!(
+            transform_module(Module::new(
+                vec![],
+                vec![FunctionDeclaration::new("g", function_type.clone())],
+                vec![],
+                vec![FunctionDefinition::new(
+                    "f",
+                    vec![],
+                    types::Primitive::PointerInteger,
+                    Block::new(
+                        vec![If::new(
+                            types::Primitive::PointerInteger,
+                            Primitive::Boolean(true),
+                            Block::new(
+                                vec![
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into()],
+                                        "a"
+                                    )
+                                    .into(),
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["x".into(), "y".into()],
+                                        "b"
+                                    )
+                                    .into()
+                                ],
+                                Branch::new(types::Primitive::PointerInteger, Variable::new("b")),
+                            ),
+                            Block::new(
+                                vec![
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into()],
+                                        "c"
+                                    )
+                                    .into(),
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["x".into(), "y".into()],
+                                        "d"
+                                    )
+                                    .into()
+                                ],
+                                Branch::new(types::Primitive::PointerInteger, Variable::new("d")),
+                            ),
+                            "e",
+                        )
+                        .into()],
+                        Return::new(types::Primitive::PointerInteger, Variable::new("e")),
+                    ),
+                    Default::default(),
+                )],
+            )),
+            Module::new(
+                vec![],
+                vec![FunctionDeclaration::new("g", function_type.clone())],
+                vec![],
+                vec![FunctionDefinition::new(
+                    "f",
+                    vec![],
+                    types::Primitive::PointerInteger,
+                    Block::new(
+                        vec![If::new(
+                            types::Primitive::PointerInteger,
+                            Primitive::Boolean(true),
+                            Block::new(
+                                vec![
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into()],
+                                        "a"
+                                    )
+                                    .into(),
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into(), "x".into()],
+                                        "b"
+                                    )
+                                    .into()
+                                ],
+                                Branch::new(types::Primitive::PointerInteger, Variable::new("b")),
+                            ),
+                            Block::new(
+                                vec![
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into()],
+                                        "c"
+                                    )
+                                    .into(),
+                                    create_call(
+                                        function_type.clone(),
+                                        Variable::new("g"),
+                                        vec![],
+                                        vec!["y".into(), "x".into()],
+                                        "d"
+                                    )
+                                    .into()
+                                ],
+                                Branch::new(types::Primitive::PointerInteger, Variable::new("d")),
+                            ),
+                            "e",
+                        )
+                        .into()],
+                        Return::new(types::Primitive::PointerInteger, Variable::new("e")),
                     ),
                     Default::default(),
                 )],
