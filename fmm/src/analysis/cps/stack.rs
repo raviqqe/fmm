fec317365b3a68bf39b90bdf114afebf4b0d8480
Yota Toyama <raviqqe@gmail.com>
2022-10-30 13:25:12 -0700

    Revert "Revert "Remove debug codes""
    This reverts commit 6920aedbbbc73dff1fe8529cce8ae1e1529a8063.

--- fmm/src/analysis/cps/stack.rs
+++ fmm/src/analysis/cps/stack.rs
@@ -119,19 +119,6 @@ pub fn partial_push(
 
         return Ok(());
     }
-    dbg!(
-        index,
-        old_elements.len(),
-        new_elements.len(),
-        old_elements
-            .iter()
-            .map(|(name, _)| name)
-            .collect::<Vec<_>>(),
-        new_elements
-            .iter()
-            .map(|(name, _)| name)
-            .collect::<Vec<_>>()
-    );
 
     extend(
         builder,
